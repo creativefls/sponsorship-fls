c95d6bc22d5b4d787a9fabf64ffcf508b510ed98
wkwksama <mailpakgasing@gmail.com>
2017-08-02 23:22:11 +0700

    buang popup notif waktu buka halaman admin

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -83,9 +83,9 @@
 
         	demo.initChartist();
 
-        	$.notify({
-            	icon: 'ti-gift',
-            	message: "Welcome to <b>Paper Dashboard</b> - a beautiful Bootstrap freebie for your next project."
+        	// $.notify({
+            // 	icon: 'ti-gift',
+            // 	message: "Welcome to <b>Paper Dashboard</b> - a beautiful Bootstrap freebie for your next project."
 
             },{
                 type: 'success',
