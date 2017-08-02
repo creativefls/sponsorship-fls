bce5783e11c7626007c23521765386d576711ee0
wkwksama <mailpakgasing@gmail.com>
2017-08-02 16:26:37 +0700

    model Sponsor: bikin get type sponsorship

--- app/Sponsor.php
+++ app/Sponsor.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Sponsor extends Model
 {
     //
+    public function getType(){
+        if ($this->type_sponsorship == 1) {
+            return "Fresh Money";
+        }else if ($this->type_sponsorship == 2) {
+            return "In-Kind";
+        }else if ($this->type_sponsorship == 3) {
+            return "Media Partner";
+        }
+    }
 }
