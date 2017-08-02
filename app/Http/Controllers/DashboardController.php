bb45bf9462ffce40a51c2c63627d4a3bb0ab995c
wkwksama <mailpakgasing@gmail.com>
2017-08-02 16:26:52 +0700

    dashboard nampilin jumlah2 sponsor

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,9 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Sponsor;
+
 class DashboardController extends Controller
 {
     function show(){
-        return view('admin.dashboard');
+        $all_sponsors = Sponsor::all()->count();
+        $fresh_money_sponsors = Sponsor::where('type_sponsorship', '1')->count();
+        $in_kind_sponsors = Sponsor::where('type_sponsorship', '2')->count();
+        $medpar_sponsors = Sponsor::where('type_sponsorship', '3')->count();
+
+        return view('admin.dashboard', compact(
+            'all_sponsors',
+            'fresh_money_sponsors',
+            'in_kind_sponsors',
+            'medpar_sponsors'
+        ));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="row">
         <div class="col-lg-3 col-sm-6">
-            MASIH STATIS
             <div class="card">
                 <div class="content">
                     <div class="row">
@@ -14,8 +13,8 @@
                         </div>
                         <div class="col-xs-7">
                             <div class="numbers">
-                                <p>Capacity</p>
-                                105GB
+                                <p>Total</p>
+                                {{ $all_sponsors }}
                             </div>
                         </div>
                     </div>
@@ -39,8 +38,8 @@
                         </div>
                         <div class="col-xs-7">
                             <div class="numbers">
-                                <p>Revenue</p>
-                                $1,345
+                                <p>Fresh Money</p>
+                                {{ $fresh_money_sponsors }}
                             </div>
                         </div>
                     </div>
@@ -64,8 +63,8 @@
                         </div>
                         <div class="col-xs-7">
                             <div class="numbers">
-                                <p>Errors</p>
-                                23
+                                <p>In-Kind</p>
+                                {{ $in_kind_sponsors }}
                             </div>
                         </div>
                     </div>
@@ -89,8 +88,8 @@
                         </div>
                         <div class="col-xs-7">
                             <div class="numbers">
-                                <p>Followers</p>
-                                +45
+                                <p>Media Partner</p>
+                                {{ $medpar_sponsors }}
                             </div>
                         </div>
                     </div>
@@ -104,32 +103,7 @@
             </div>
         </div>
     </div>
-    <div class="row">
-
-        <div class="col-md-12">
-            <div class="card">
-                <div class="header">
-                    <h4 class="title">Users Behavior</h4>
-                    <p class="category">24 Hours performance</p>
-                </div>
-                <div class="content">
-                    <div id="chartHours" class="ct-chart"></div>
-                    <div class="footer">
-                        <div class="chart-legend">
-                            <i class="fa fa-circle text-info"></i> Open
-                            <i class="fa fa-circle text-danger"></i> Click
-                            <i class="fa fa-circle text-warning"></i> Click Second Time
-                        </div>
-                        <hr>
-                        <div class="stats">
-                            <i class="ti-reload"></i> Updated 3 minutes ago
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-md-6">
             <div class="card">
                 <div class="header">
@@ -153,28 +127,6 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="card ">
-                <div class="header">
-                    <h4 class="title">2015 Sales</h4>
-                    <p class="category">All products including Taxes</p>
-                </div>
-                <div class="content">
-                    <div id="chartActivity" class="ct-chart"></div>
-
-                    <div class="footer">
-                        <div class="chart-legend">
-                            <i class="fa fa-circle text-info"></i> Tesla Model S
-                            <i class="fa fa-circle text-warning"></i> BMW 5 Series
-                        </div>
-                        <hr>
-                        <div class="stats">
-                            <i class="ti-check"></i> Data information certified
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    </div> --}}
 
 @endsection
